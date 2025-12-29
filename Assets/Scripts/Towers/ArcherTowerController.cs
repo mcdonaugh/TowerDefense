09317d669ad93e3000f5bdfa4c7e229340f0140c
mcdonaugh <brianmcdonaugh@gmail.com>
2025-12-29 09:57:19 -0500

    Added Archer rotation on Archer Tower

--- Assets/Scripts/Towers/ArcherTowerController.cs
+++ Assets/Scripts/Towers/ArcherTowerController.cs
@@ -11,4 +11,22 @@ public class ArcherTowerController : BaseTower
         _archer.transform.SetParent(transform);
         _archer.transform.localPosition = new Vector3(0,4,0);
     }
+
+    private void Update()
+    {
+        if(_targetQueue.Count > 0)
+        {
+        Vector3 direction = _targetQueue[0].transform.position - _archer.transform.position;
+        Debug.DrawLine(_archer.transform.position, _targetQueue[0].transform.position, Color.red);
+        Debug.DrawLine(_targetQueue[0].transform.position, new Vector3(_targetQueue[0].transform.position.x,_targetQueue[0].transform.position.y,_archer.transform.position.z), Color.yellow);
+        Debug.DrawLine(_archer.transform.position, new Vector3(_targetQueue[0].transform.position.x,_targetQueue[0].transform.position.y,_archer.transform.position.z), Color.blue);
+
+        float angle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg;
+        
+        Debug.Log(angle);
+
+        _archer.transform.rotation = Quaternion.Euler(0,angle,0);
+        
+        }
+    }
 }
