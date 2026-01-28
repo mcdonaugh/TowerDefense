86718619144bf27182267307caefce0e392e415e
mcdonaugh <brianmcdonaugh@gmail.com>
2026-01-28 07:14:00 -0500

    Added basic start menu that starts the game and enables both enemy spawning and game HUD

--- Assets/Scripts/GameStateController.cs
+++ Assets/Scripts/GameStateController.cs
@@ -1,6 +1,32 @@
+using System;
 using UnityEngine;
 
 public class GameStateController : MonoBehaviour
 {
+    [SerializeField] private Canvas _mainMenuUI;
+    [SerializeField] private Canvas _gameUI;
+    [SerializeField] private EnemySpawnerController _enemySpawnerController;
+    private bool _gameIsActive;
+    private void Awake()
+    {
+        ResetGame();
+    }
 
+    public void StartGame()
+    {
+        _gameIsActive = true;
+        _gameUI.gameObject.SetActive(true);
+        _enemySpawnerController.gameObject.SetActive(true);
+
+        _mainMenuUI.gameObject.SetActive(false);
+    }
+
+    private void ResetGame()
+    {
+        _gameIsActive = false;
+        _gameUI.gameObject.SetActive(false);
+        _enemySpawnerController.gameObject.SetActive(false);
+
+        _mainMenuUI.gameObject.SetActive(true);
+    }
 }
